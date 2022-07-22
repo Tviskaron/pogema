45d124e5640b4a1201c6708f246f7befacdff32c
tviskaron <tviskaron@gmail.com>
2022-07-22 14:54:19 +0300

    bump version to 1.1.0

--- pogema/__init__.py
+++ pogema/__init__.py
@@ -1,30 +1,32 @@
 from gym import register
 from pogema.grid_config import GridConfig
+from pogema.integrations.make_pogema import pogema_v0
 
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 __all__ = [
     'GridConfig',
+    'pogema_v0'
 ]
 
 register(
     id="Pogema-v0",
-    entry_point="pogema.integrations.make_pogema:make_pogema",
+    entry_point="pogema.integrations.make_pogema:make_single_agent_gym",
 )
 
 
-def _get_num_agents_by_target_density(_size, _agent_density, _obstacle_density):
-    return round(_agent_density * (_size * _size * (1.0 - _obstacle_density)))
+# def _get_num_agents_by_target_density(_size, _agent_density, _obstacle_density):
+#     return round(_agent_density * (_size * _size * (1.0 - _obstacle_density)))
 
 
-for size, max_episode_steps in zip([8, 16, 32, 64], [64, 128, 256, 512]):
-    for obstacle_density in [0.3]:
-        for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'extra-hard'],
-                                             [0.0223, 0.0446, 0.0892, 0.1784]):
-            num_agents = _get_num_agents_by_target_density(size, agent_density, obstacle_density)
-            register(
-                id=f'Pogema-{size}x{size}-{difficulty}-v0',
-                entry_point="pogema.integrations.make_pogema:make_pogema",
-                kwargs={"grid_config": GridConfig(size=size, num_agents=num_agents, density=obstacle_density,
-                                                  max_episode_steps=max_episode_steps),
-                        "integration": None})
+# for size, max_episode_steps in zip([8, 16, 32, 64], [64, 128, 256, 512]):
+#     for obstacle_density in [0.3]:
+#         for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'extra-hard'],
+#                                              [0.0223, 0.0446, 0.0892, 0.1784]):
+#             num_agents = _get_num_agents_by_target_density(size, agent_density, obstacle_density)
+#             register(
+#                 id=f'Pogema-{size}x{size}-{difficulty}-v0',
+#                 entry_point="pogema.integrations.make_pogema:make_pogema",
+#                 kwargs={"grid_config": GridConfig(size=size, num_agents=num_agents, density=obstacle_density,
+#                                                   max_episode_steps=max_episode_steps),
+#                         "integration": None})
